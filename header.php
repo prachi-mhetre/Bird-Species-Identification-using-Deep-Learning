2effd19047d347e55c0dcaaae20157018d458b85
prachi-mhetre <33054927+prachi-mhetre@users.noreply.github.com>
2019-05-14 15:29:56 +0530

    header

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bird Species Identification using Deep Learning</title>
+	<link href="css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="css/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom CSS -->
+    <link href="css/css/stylish-portfolio.css" rel="stylesheet">
+</head>
+<body>
+
+<!-- Navigation -->
+    <a id="menu-toggle" href="index.php" class="btn btn-dark btn-lg toggle">
+      <i class="fa fa-bars"></i>
+    </a>
+
+    <!-- Header -->
+    <header class="header" id="top">
+      <div class="text-vertical-center">
+        <h1>Bird Species Identification using Deep Learning</h1>
+        <h3>Intelligent Bird Recognition</h3>
+        <br>
+        
+      </div>
+    </header>
+
+   
+
+    
