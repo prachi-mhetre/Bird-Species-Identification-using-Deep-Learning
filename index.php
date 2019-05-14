d89d736393700cf56c1d3740619ed0721c56a9b1
prachi-mhetre <33054927+prachi-mhetre@users.noreply.github.com>
2019-05-14 15:31:29 +0530

    php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+include_once("header.php");
+?>
+
+    <section id="services" class="services bg-primary text-white">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-lg-10 mx-auto">
+            <h2>Our Services</h2>
+            <hr class="small">
+            <div class="row">
+              <div class="col-md-12 col-sm-6 col-offset-md-6 offset-sm-5">
+                <div class="service-item ">
+                  <span class="fa-stack fa-4x">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-cloud fa-stack-1x text-primary"></i>
+                  </span>
+                  <h4>
+                    <strong>Bird Recognition</strong>
+                  </h4>
+                  <p>Submit an image, identify the bird</p>
+                  <a class="btn btn-warning" href="upload.php">Upload Image</a>
+                  
+                </div>
+              </div>
+              
+            </div>
+            <!-- /.row (nested) -->
+          </div>
+          <!-- /.col-lg-10 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    
+<?php
+include_once("footer.php");
+?>
